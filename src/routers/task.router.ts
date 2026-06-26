8a6176bc9c12ad762ebe500597f5c49af147eda7
EstelleTelematica <vighettoestelle@telematicainformatica.it>
2026-06-26 15:03:17 +0200

    feat: implement task retrieval

--- src/routers/task.router.ts
+++ src/routers/task.router.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { getAllTasks } from "../controllers/task.controller";
+import { decode } from "../middlewares/decode";
 
 const router = express.Router();
 
-router.get('/', getAllTasks);
+router.get('/', decode, getAllTasks); //decode è una call back perché viene passata come parametro e lo so perhé non ha le tonde
 
 //router.get('/:id', getOneTask);
 
